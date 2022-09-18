ed3ce1ba6d435e56aea41536a394edad5f519076
bolgheroni <robertobolgheroni@usp.br>
2022-09-17 22:25:58 -0300

    Add read_arb function implementation

--- PA0/asgt.cpp
+++ PA0/asgt.cpp
@@ -3,11 +3,25 @@
  */
 
 #include "asgt.h"
+#include <vector>                      // for std::vector
+#include <utility>                     // for std::pair
 
 Arb read_arb(std::istream& in)
 {
-  Arb arb(0);
-  return arb;
+  typename boost::graph_traits<Arb>::vertices_size_type num_vertices; in >> num_vertices;
+
+  Arb arb(num_vertices);
+  size_t num_edges = num_vertices - 1;
+
+  // accumulate all data before graph construction
+  std::vector<std::pair<int, int> > edges;
+  while (num_edges--) {
+    int u, v; in >> u >> v;
+    edges.push_back(std::make_pair(--u, --v));
+  }
+
+  // guarantees copy elision in c++17
+  return Arb (edges.cbegin(), edges.cend(), num_vertices);
 }
 
 HeadStart preprocess (Arb &arb, const Vertex& root)
