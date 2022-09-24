e69a9c81b57674805121fa5c61d79ec33743ceb1
Roberto Bolgheroni <robertobolgheroni@usp.br>
2022-09-24 17:56:27 -0300

    Implement preproccess and is_ancestor functions with dfs

--- PA0/asgt.cpp
+++ PA0/asgt.cpp
@@ -3,33 +3,73 @@
  */
 
 #include "asgt.h"
-#include <vector>                      // for std::vector
-#include <utility>                     // for std::pair
+#include <vector>  // for std::vector
+#include <utility> // for std::pair
 
-Arb read_arb(std::istream& in)
+enum Color
 {
-  typename boost::graph_traits<Arb>::vertices_size_type num_vertices; in >> num_vertices;
+  white,
+  grey,
+  black
+};
+
+
+Arb read_arb(std::istream &in)
+{
+  typename boost::graph_traits<Arb>::vertices_size_type num_vertices;
+  in >> num_vertices;
 
   Arb arb(num_vertices);
   size_t num_edges = num_vertices - 1;
 
   // accumulate all data before graph construction
-  std::vector<std::pair<int, int> > edges;
-  while (num_edges--) {
-    int u, v; in >> u >> v;
+  std::vector<std::pair<int, int>> edges;
+  while (num_edges--)
+  {
+    int u, v;
+    in >> u >> v;
     edges.push_back(std::make_pair(--u, --v));
   }
 
   // guarantees copy elision in c++17
-  return Arb (edges.cbegin(), edges.cend(), num_vertices);
+  return Arb(edges.cbegin(), edges.cend(), num_vertices);
 }
 
-HeadStart preprocess (Arb &arb, const Vertex& root)
+
+int dfs(Arb &arb, const Vertex &current, std::vector<int> &d, std::vector<int> &f, std::vector<int> &colours, int time)
 {
-  return HeadStart(42);
+  colours[current] = grey;
+  d[current] = ++time;
+  std::for_each(boost::out_edges(current, arb).first,
+                                boost::out_edges(current, arb).second,
+                                [&](const auto& arc) {
+                                  auto targetV = boost::target(arc, arb); 
+                                  if (colours[targetV] == white) {
+                                    time = dfs(arb, targetV, d, f, colours, time);
+                                  }
+                                });
+  colours[current] = black;
+  f[current] = ++time;
+  return time; 
 }
 
-bool is_ancestor (const Vertex& u, const Vertex& v, const HeadStart& data)
+HeadStart preprocess(Arb &arb, const Vertex &root)
 {
-  return false;
+  int v_length = 0;
+  std::for_each(boost::vertices(arb).first,
+                boost::vertices(arb).second,
+                [&](const auto &vertex)
+                {
+                  v_length++;
+                });
+
+  auto ret = HeadStart(v_length);
+  std::vector<int> colours(v_length, white);
+  dfs(arb, root, ret.d, ret.f, colours, 0);
+  return ret;
+}
+
+bool is_ancestor(const Vertex &u, const Vertex &v, const HeadStart &data)
+{
+  return data.d[u] <= data.d[v] && data.f[v] <= data.f[u];
 }
