c8fa76e42e4b1f3ef30168c2f83f29e46d01299b
Roberto Bolgheroni <robertobolgheroni@usp.br>
2022-10-17 23:53:00 -0300

    Remove incorrect header from main.cpp

--- PA1/main.cpp
+++ PA1/main.cpp
@@ -1,8 +1,3 @@
-/* This is the driver. Code similar to this will be used for grading.
- * It may be edited by the students for testing purposes, however it
- * WILL NOT be submitted as part of the assignment.
- */
-
 #include <iostream>
 #define BOOST_ALLOW_DEPRECATED_HEADERS // silence warnings
 #include <boost/graph/graph_traits.hpp>
